3440f2be90aa897c548b1a5a7af33cf010762c79
Tomasz <pengwius@protonmail.ch>
2024-10-22 15:58:52 +0200

    mongodb

--- Package.swift
+++ Package.swift
@@ -11,6 +11,8 @@ let package = Package(
         .package(url: "https://github.com/vapor/vapor.git", from: "4.99.3"),
         // 🔵 Non-blocking, event-driven networking for Swift. Used for custom executors
         .package(url: "https://github.com/apple/swift-nio.git", from: "2.65.0"),
+        .package(url: "https://github.com/vapor/fluent.git", from: "4.0.0"),
+        .package(url: "https://github.com/vapor/fluent-mongo-driver.git", from: "1.0.0"),
     ],
     targets: [
         .executableTarget(
@@ -19,6 +21,9 @@ let package = Package(
                 .product(name: "Vapor", package: "vapor"),
                 .product(name: "NIOCore", package: "swift-nio"),
                 .product(name: "NIOPosix", package: "swift-nio"),
+                .product(name: "Fluent", package: "fluent"),
+                .product(name: "FluentMongoDriver", package: "fluent-mongo-driver"),
+                .product(name: "Vapor", package: "vapor"),
             ],
             swiftSettings: swiftSettings
         ),
--- Sources/App/configure.swift
+++ Sources/App/configure.swift
@@ -1,9 +1,15 @@
 import Vapor
+import Fluent
+import FluentMongoDriver
+
 
 // configures your application
 public func configure(_ app: Application) async throws {
     // uncomment to serve files from /Public folder
     // app.middleware.use(FileMiddleware(publicDirectory: app.directory.publicDirectory))
     // register routes
+
+    // try app.databases.use(.mongo(connectionString: "<connection string>"), as: .mongo)
+
     try routes(app)
 }
