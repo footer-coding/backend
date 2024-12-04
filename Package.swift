9bd592e00f0a600b4d6b9b81655fb2e637f2ed30
kube3qq <skuba684@gmail.com>
2024-12-04 16:03:35 +0100

    stripe

--- Package.swift
+++ Package.swift
@@ -14,7 +14,7 @@ let package = Package(
         .package(url: "https://github.com/vapor/fluent.git", from: "4.0.0"),
         .package(url: "https://github.com/vapor/fluent-mongo-driver.git", from: "1.0.0"),    
         .package(url: "https://github.com/swiftpackages/DotEnv.git", from: "3.0.0"),
-        .package(url: "https://github.com/vapor/jwt.git", from: "5.0.0-rc"),
+        .package(url: "https://github.com/vapor-community/stripe-kit.git", from: "22.0.0")
     ],
     targets: [
         .executableTarget(
@@ -27,7 +27,7 @@ let package = Package(
                 .product(name: "FluentMongoDriver", package: "fluent-mongo-driver"),
                 .product(name: "Vapor", package: "vapor"),
                 .product(name: "DotEnv", package: "DotEnv"),
-                .product(name: "JWT", package: "jwt")
+                .product(name: "StripeKit", package: "stripe-kit")
             ],
             swiftSettings: swiftSettings
         ),
--- Sources/App/configure.swift
+++ Sources/App/configure.swift
@@ -2,13 +2,77 @@ import Vapor
 import Fluent
 import FluentMongoDriver
 import DotEnv
-import JWT
+import StripeKit
+
+extension Application {
+    public var stripe: StripeClient {
+        guard let stripeKey = Environment.get("STRIPE_PV_KEY") else {
+            fatalError("STRIPE_API_KEY env var required")
+        }
+        return .init(httpClient: self.http.client.shared, apiKey: stripeKey)
+    }
+}
+
+extension Request {
+    private struct StripeKey: StorageKey {
+        typealias Value = StripeClient
+    }
+    
+    public var stripe: StripeClient {
+        if let existing = application.storage[StripeKey.self] {
+            return existing
+        } else {
+            guard let stripeKey = Environment.get("STRIPE_PV_KEY") else {
+                fatalError("STRIPE_API_KEY env var required")
+            }
+            let new = StripeClient(httpClient: self.application.http.client.shared, apiKey: stripeKey)
+            self.application.storage[StripeKey.self] = new
+            return new
+        }
+    }
+}
+
+extension StripeClient {
+    /// Verifies a Stripe signature for a given `Request`. This automatically looks for the header in the headers of the request and the body.
+    /// - Parameters:
+    ///     - req: The `Request` object to check header and body for
+    ///     - secret: The webhook secret used to verify the signature
+    ///     - tolerance: In seconds the time difference tolerance to prevent replay attacks: Default 300 seconds
+    /// - Throws: `StripeSignatureError`
+    public static func verifySignature(for req: Request, secret: String, tolerance: Double = 300) throws {
+        guard let header = req.headers.first(name: "Stripe-Signature") else {
+            throw StripeSignatureError.unableToParseHeader
+        }
+        
+        guard let data = req.body.data else {
+            throw StripeSignatureError.noMatchingSignatureFound
+        }
+        
+        try StripeClient.verifySignature(payload: Data(data.readableBytesView), header: header, secret: secret, tolerance: tolerance)
+    }
+}
+
+extension StripeSignatureError: AbortError {
+    public var reason: String {
+        switch self {
+        case .noMatchingSignatureFound:
+            return "No matching signature was found"
+        case .timestampNotTolerated:
+            return "Timestamp was not tolerated"
+        case .unableToParseHeader:
+            return "Unable to parse Stripe-Signature header"
+        }
+    }
+    
+    public var status: HTTPResponseStatus {
+        .badRequest
+    }
+}
 
 
 // configures your application
 public func configure(_ app: Application) async throws {
-    // uncomment to serve files from /Public folder
-    // app.middleware.use(FileMiddleware(publicDirectory: app.directory.publicDirectory))
+
     let path = "./.env"
     let env = try DotEnv.read(path: path)
     env.lines 
@@ -30,51 +94,10 @@ public func configure(_ app: Application) async throws {
     
      try app.databases.use(.mongo(connectionString: mongoUrl), as: .mongo)
 
-    
-    
-     app.migrations.add(CreateUser())
-    try await app.autoMigrate()
+     app.stripe
 
-     let path = "./.env"
-    let env = try DotEnv.read(path: path)
-    env.lines 
-    env.load()
-    print(ProcessInfo.processInfo.environment["CLERK_SK"]) 
-
-    var logger = Logger(label: "key")
-    logger.logLevel = .trace
-
-    let logLevel = Environment.get("LOG_LEVEL")
-    
-    if let logLevel, let logLevel = Logger.Level(rawValue: logLevel) {
-        logger.logLevel = logLevel
-    }
-
-    let clerkSK: String = ProcessInfo.processInfo.environment["CLERK_SK"] 
-
-    let pem = """
-    -----BEGIN PUBLIC KEY-----
-    MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAsAjc1Pt7Itp7BrEUzd2B
-    sdo8Yv0/OTzI/4ZNsRfW1n5qaLBJg/ZsPmwlSRGOx2cN1Lk+5U8lHZYDSuoqSHGz
-    kslvYAoMKtLEaI1PZqyQjFgVlvpa6m5PN52a6Cu5fnzliqm6LVVnn3H3ATFinCgh
-    +loOH8yzB7gxn9DNFljLllnEqrrgvQD5g3c5yUHF5Pn6ZASBifNsc0RdBhrRMQ2j
-    zbUxbNUggUBM9kJom2uFIlJQaPLjTg0yt8NCPkbiI0UquOowBN9N0QXBD34dIKTJ
-    niFaRm9TWvyHVr+Q+nDL3ipas6xEjsTr+QxOj0FMTr2VfddB0FWTkaB1uTbyCiCo
-    6wIDAQAB
-    -----END PUBLIC KEY-----
-    """
-
-    // Initialize an RSA key with public pem.
-    let key = try Insecure.RSA.PublicKey(pem: pem)
-
-    await app.jwt.keys.add(rsa: clerkSK, digestAlgorithm: .sha256)
-
-
-    //regitser controllers
-    //try app.register(collection: UserController())
-    // await app.jwt.keys.add(rsa: , digestAlgorithm: .sha256)
-
-    //app.middleware.use(MyMiddleware())
+     
+     app.middleware.use(FileMiddleware(publicDirectory: app.directory.publicDirectory))
 
     // register routes
     try routes(app)
