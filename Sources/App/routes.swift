f076719863e2e2f784cc481a43f6757423436b24
Tomasz <pengwius@protonmail.ch>
2024-12-05 22:44:07 +0100

    Create payment intent for iOS app

--- Sources/App/routes.swift
+++ Sources/App/routes.swift
@@ -8,7 +8,6 @@ func routes(_ app: Application) throws {
     }            
 
     app.get("test") { req -> Response in
-
         let filePath = app.directory.publicDirectory + "Client/index.html"
         return req.fileio.streamFile(at: filePath)
     }
@@ -20,6 +19,8 @@ func routes(_ app: Application) throws {
 
     app.post("create-payment-intent") { req async throws -> Response in
         let items = try req.content.decode([String: [Item]].self)["items"] ?? []
+
+        print(items)
         
         let amount = calculateOrderAmount(items: items)
         
@@ -58,19 +59,57 @@ func routes(_ app: Application) throws {
         
         let response = [
             "clientSecret": paymentIntent.clientSecret,
+            "paymentIntent": paymentIntent.clientSecret,
             "dpmCheckerLink": "https://dashboard.stripe.com/settings/payment_methods/review?transaction_id=\(paymentIntent.id)"
         ]
         
         return Response(status: .ok, body: .init(data: try JSONEncoder().encode(response)))
-
     }
-    // app.post("verify") {req -> String in
-    //     let user = try req.auth.require(JWTModel.self)
 
-    //     print(user)
-
-    //     return "dziala"
-    // }
+    app.post("create-payment-sheet") { req async throws -> Response in
+        let items = try req.content.decode([String: [Item]].self)["items"] ?? []
+        
+        let amount = calculateOrderAmount(items: items)
+        
+        let paymentIntent = try await req.application.stripe.paymentIntents.create(
+            amount: amount,
+            currency: .pln,
+            automaticPaymentMethods: ["enabled": true],
+            confirm: false,
+            customer: nil,
+            description: nil,
+            metadata: nil,
+            offSession: nil,
+            paymentMethod: nil,
+            receiptEmail: nil,
+            setupFutureUsage: nil,
+            shipping: nil,
+            statementDescriptor: nil,
+            statementDescriptorSuffix: nil,
+            applicationFeeAmount: nil,
+            captureMethod: nil,
+            confirmationMethod: nil,
+            errorOnRequiresAction: nil,
+            mandate: nil,
+            mandateData: nil,
+            onBehalfOf: nil,
+            paymentMethodData: nil,
+            paymentMethodOptions: nil,
+            paymentMethodTypes: ["card"],
+            radarOptions: nil,
+            returnUrl: nil,
+            transferData: nil,
+            transferGroup: nil,
+            useStripeSDK: nil,
+            expand: nil
+        )
+        
+        let response = [
+            "paymentIntent": paymentIntent.clientSecret
+        ]
+        
+        return Response(status: .ok, body: .init(data: try JSONEncoder().encode(response)))
+    }
 
     app.post("addToDb") { req -> String in
         //print JWT token from request
